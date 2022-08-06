5b4f78a17c021651fca476bc7f1f343270e24a12
Ghawken <ghawken@hotkey.net.au>
2022-08-06 15:28:04 +1000

    0.3.16 Pyhap library updated: Avoid parsing URL twice. Speed increase.
    0.3.15
    Add check for version change and publish details of xattr terminal command to log to enable copy & paste (accidentally closed my terminal window which had it saved - and was a pain to find again!)
    Remove some beta level windows.
    
    0.3.14
    Change how GarageDoor Setter works -- use binaryInput1 preferentially if exists, and then onOffState

--- HomeKitLink-Siri.indigoPlugin/Contents/Server Plugin/pyhap/hap_handler.py
+++ HomeKitLink-Siri.indigoPlugin/Contents/Server Plugin/pyhap/hap_handler.py
@@ -146,7 +146,7 @@ class HAPServerHandler:
         self.command = None
         self.headers = None
         self.request_body = None
-
+        self.parsed_url = None
         self.response = None
 
     def _set_encryption_ctx(
@@ -200,6 +200,7 @@ class HAPServerHandler:
         self.command = request.method.decode()
         self.headers = {k.decode(): v.decode() for k, v in request.headers}
         self.request_body = body
+        self.parsed_url = urlparse(self.path)
         response = HAPResponse()
         self.response = response
 
@@ -210,8 +211,8 @@ class HAPServerHandler:
             self.path,
             self.headers,
         )
-
-        path = urlparse(self.path).path
+        path = self.parsed_url.path
+        #path = urlparse(self.path).path
         try:
             getattr(self, self.HANDLERS[self.command][path])()
         except UnprivilegedRequestException:
@@ -585,7 +586,8 @@ class HAPServerHandler:
             raise UnprivilegedRequestException
 
         # Check that char exists and ...
-        params = parse_qs(urlparse(self.path).query)
+        #params = parse_qs(urlparse(self.path).query)
+        params = parse_qs(self.parsed_url.query)
         response = self.accessory_handler.get_characteristics(
             params["id"][0].split(",")
         )
